fc6a1dda1bf5b21121a2043817453070e64588aa
azhar2006-collab <azhar12082006@gmail.com>
2026-02-10 23:23:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', e => {
-    const href = link.getAttribute('href');
-    // Only prevent default for hash links (#), allow normal navigation for page links
-    if (href.startsWith('#')) {
-      e.preventDefault();
-      const target = document.querySelector(href);
-      if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  });
-});
