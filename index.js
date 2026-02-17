8762babc810d1642af80736ad89597928f44ac24
azhar2006-collab <azhar12082006@gmail.com>
2026-02-17 21:53:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const menu = document.getElementById("menu");
+const nav = document.getElementById("nav");
+
+if (menu) {
+    menu.addEventListener("click", () => {
+        nav.style.display =
+            nav.style.display === "flex" ? "none" : "flex";
+    });
+}
+
+function Menu() {
+    document.getElementById("nav");
+    nav.classList.toggle("show");
+    
+
+}
+
